b6fc3944006b15e5d3be7cba37cd2f79c5ccd953
banset <mohitbasnet80@gmail.com>
2024-03-06 23:16:21 +0545

    User auth registration and login

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -40,7 +40,9 @@ THIRD_PARTY_APPS = [
     "rest_framework",
     "django_filters",
     "django_countries",
-    "phonenumber_field"
+    "phonenumber_field",
+    "djoser",
+    "rest_framework_simplejwt"
 ]
 
 LOCAL_APPS = [
@@ -133,6 +135,47 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAUL_AUTHENTICATION_CLASSES":(
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+
+}
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES":(
+        "Bearer",
+        "JWT",
+    ),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days = 1),
+    'SIGNING_KEY': env("SIGNING_KEY"),
+    'AUTH_HEADER_NAME': "HTTP_AUTHORIZATION",
+    'AUTH_TOKEN_CLASSES':("rest_framework_simplejwt.tokens.AccessToken",),
+
+}
+
+DJOSER = {
+     "LOGIN_FIELD":"email",
+     "USER_CREATED_PASSWORD_RETYPE": True,
+     "USER_CHANGED_EMAIL_CONFIRMATION": True,
+     "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+     "SEND_CONFIRMATION_EMAIL": True,
+     "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+     "SET_PASSWORD_RETYPE": True,
+     "PASSWORD_RESET_CONFIRM_RETYPE": True,
+     "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
+     "ACTIVATION_URL":"activate/{uid}/{token}",
+     "SEND_ACTIVATION_EMAIL": True,
+     "SERIALIZERS":{
+        'user_create': 'apps.users.serializers.CreateUserSerializer',
+        'user': 'apps.users.serializers.UserSerializer',
+        'current_user': "apps.users.serializers.UserSerializer",
+        'user_delete': "djoser.serializers.UserDeleteSerializer",
+     },
+}
 
 AUTH_USER_MODEL = "users.User"
 
