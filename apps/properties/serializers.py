b9d033c1b1431e811ab639deea7a83b6c26844d7
banset <mohitbasnet80@gmail.com>
2024-03-16 23:10:36 +0545

    Added crud funtionality for property

--- apps/properties/serializers.py
+++ apps/properties/serializers.py
@@ -4,12 +4,18 @@ from rest_framework import serializers
 
 from .models import Property, PropertyViews
 
+
 class PropertySerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
-    country = CountryField(name_only = True)
+    country = CountryField(name_only=True)
+    cover_photo = serializers.SerializerMethodField()
+    profile_photo = serializers.SerializerMethodField()
+    photo1 = serializers.SerializerMethodField()
+    photo2 = serializers.SerializerMethodField()
+    photo3 = serializers.SerializerMethodField()
+    photo4 = serializers.SerializerMethodField()
 
     class Meta:
-
         model = Property
         fields = [
             "id",
@@ -41,17 +47,35 @@ class PropertySerializer(serializers.ModelSerializer):
             "published_status",
             "views",
         ]
-    
-    def get_user(self,obj):
+
+    def get_user(self, obj):
         return obj.user.username
+
+    def get_cover_photo(self, obj):
+        return obj.cover_photo.url
+
+    def get_photo1(self, obj):
+        return obj.photo1.url
+
+    def get_photo2(self, obj):
+        return obj.photo2.url
+
+    def get_photo3(self, obj):
+        return obj.photo3.url
+
+    def get_photo4(self, obj):
+        return obj.photo4.url
+
+    def get_profile_photo(self, obj):
+        return obj.user.profile.profile_photo.url
 
 
 class PropertyCreateSerializer(serializers.ModelSerializer):
-    country = CountryField(name_only = True)
+    country = CountryField(name_only=True)
 
     class Meta:
         model = Property
-        exclude = ["updated_at" , "pkid"]
+        exclude = ["updated_at", "pkid"]
 
 
 class PropertyViewSerializer(serializers.ModelSerializer):
